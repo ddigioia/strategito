a53a979b08322d482144ba549fd75a9df6a51a49
ddigioia <derekdigioia@gmail.com>
2016-10-09 16:23:15 -0400

    format chat.js to standard

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,10 +1,10 @@
 var socket = io()
-$('form').submit(function(){
+$('form').submit(function () {
   socket.emit('chat message', $('#m').val())
   $('#m').val('')
   return false
 })
-socket.on('chat message', function(msg){
+socket.on('chat message', function (msg) {
   $('#messages').append($('<li>').text(msg))
   scrollBottomOfChat()
 })
