411b06bcbba7a9bd8460830ab909ea76842a3ecd
Derek DiGioia <derekdigioia@Dereks-MacBook-Pro-8.local>
2016-10-12 16:11:05 -0400

    remove unnecessary scripts from index.html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,10 @@
+// javascript
 import './jsboard.js'
 import './index.js'
 import 'socket.io-client'
 import './chat.js'
+
+// styles
+import '../stylesheets/board.css'
+import '../stylesheets/chat.css'
+import '../stylesheets/main.css'
